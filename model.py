03bfb7171b6324b538230817339a7ce1ea882234
axer-me <axerm8@gmail.com>
2025-06-15 20:52:43 +0300

    html styles adjusted + added role system

--- model.py
+++ model.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 import preprocessing
 from xgboost import XGBRegressor
 
-
+table_html = None
 data_path = r'my_resourses/Sells.xlsx'
-data = pd.read_excel(data_path)
-data_preprocessed = preprocessing.transform(data)
+def load_df():
+    data = pd.read_excel(data_path)
+    global table_html
+    table_html= data.sample(10).to_html(classes="dataframe")
+    return data
+
+
+data_preprocessed = preprocessing.transform(load_df())
 
 data_scaled = preprocessing.scaling(data_preprocessed)
 
@@ -26,3 +33,17 @@ s_sample = 0.8
 prediction_model = XGBRegressor(colsample_bytree = cs_bytree, gamma = gam, learning_rate = lr,
                      max_depth = m_depth, n_estimators = n_est, subsample = s_sample)
 prediction_model.fit(X_train, y_train)
+
+group8 = data_preprocessed.loc[data_preprocessed['Категория товара'] != -1, :].groupby(['Категория товара'])[
+        ['Продажи, кг']].sum()
+x = list(group8.index)
+for i in range(group8['Продажи, кг'].count()):
+    x[i] = str(x[i])
+y = list()
+for i in range(group8['Продажи, кг'].count()):
+    y.append(group8.values[i, 0])
+plt.bar(x, y, label='Продажи по категориям')
+plt.xlabel('Категория товара')
+plt.ylabel('Продажи, 10*млн кг')
+plt.title('Столбчатая диаграмма продаж')
+plt.savefig('static/graph.png')
--- users.py
+++ users.py
@@ -9,6 +9,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(100), nullable=False)
+    about_me = db.Column(db.Text, nullable=True)
 
     def __repr__(self):
         return f'User({self.id}, {self.email}, {self.name})'
