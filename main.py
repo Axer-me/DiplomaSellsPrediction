9aea367326d12a2dfa113b9783f9db709a0ebda7
axer-me <axerm8@gmail.com>
2025-06-15 22:32:50 +0300

    html styles adjusted + added role system

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from flask_login import LoginManager, login_user, login_required, logout_user, c
 
 print(model.prediction_model.score(model.X_test, model.y_test))
 print(123)
+print('git')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'diploma-work'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
